2a1ecc226315901c930c806f5e7810bef26a7861
Stiven Garzon <usg210802@outlook.com>
2025-03-08 22:28:54 -0500

    Crud Libro

--- django_libreria/api_libreria/urls.py
+++ django_libreria/api_libreria/urls.py
@@ -2,17 +2,16 @@
 from django.urls import path
 # Importa las vistas desde el módulo views de la aplicación actual
 from .views import (
-    ConsultarLibro,BuscarLibro,
+    ConsultarLibro,BuscarLibro,CrearLibro, EliminarLibro, ActualizarLibro,
     ConsultarAutor, CrearAutor,EliminarAutor,ActualizarAutor,
     CrearEditorial,ConsultarEditorial,EliminarEditorial,ActualizarEditorial,
-    CrearLibro,CrearMiembro,CrearPrestamo
+    CrearMiembro,CrearPrestamo
 )
 
 # Define la lista de patrones de URL para la aplicación 'api_app'
 urlpatterns = [
     # Rutas para la gestión de libros
     # Lista de libros
-    path('librerias/', BuscarLibro.as_view(), name='librerias-list'),
     path('prestamo/', ConsultarLibro.as_view(), name='libro-prestamo'),
     #CRUD AUTOR
     path('autor/',ConsultarAutor.as_view(), name='consultar_autores'),
@@ -25,7 +24,12 @@ urlpatterns = [
     path('editorial/<int:id_editorial>/eliminar/',EliminarEditorial.as_view(), name='eliminar_editoriales'),
     path('editorial/<int:id_editorial>/actualizar/',ActualizarEditorial.as_view(), name='actualizar_Editoriales'),
     #CRUD LIBRO
-    path('libro/', CrearLibro.as_view(), name='libros'),
+    path('librerias/', BuscarLibro.as_view(), name='librerias-list'),
+    path('libro/', ConsultarLibro.as_view(), name='consultar_libros'),
+    path('libro/', CrearLibro.as_view(), name='crear_libros'),
+    path('libro/<int:id_libro>/eliminar/',EliminarLibro.as_view(), name='eliminar_libros'),
+    path('libro/<int:id_libro>/actualizar/',ActualizarLibro.as_view(), name='actualizar_libros'),
+
     path('miembro/', CrearMiembro.as_view(), name='miembros'),
     path('prestamo/', CrearPrestamo.as_view(), name='prestamo')
     ]
